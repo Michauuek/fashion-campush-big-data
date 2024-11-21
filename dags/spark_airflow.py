78098ee01bb902c13218358fa9e6d6f7d5ea7a22
Michauuek <michal.rzemieniuk@gmail.com>
2024-11-21 19:30:59 +0100

    Load to bronze stage

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -6,7 +6,7 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 dag = DAG(
     dag_id = "sparking_flow",
     default_args = {
-        "owner": "Yusuf Ganiyu",
+        "owner": "Michal Rz",
         "start_date": airflow.utils.dates.days_ago(1)
     },
     schedule_interval = "@daily"
@@ -25,20 +25,20 @@ python_job = SparkSubmitOperator(
     dag=dag
 )
 
-scala_job = SparkSubmitOperator(
-    task_id="scala_job",
-    conn_id="spark-conn",
-    application="jobs/scala/target/scala-2.12/word-count_2.12-0.1.jar",
-    dag=dag
-)
-
-java_job = SparkSubmitOperator(
-    task_id="java_job",
-    conn_id="spark-conn",
-    application="jobs/java/spark-job/target/spark-job-1.0-SNAPSHOT.jar",
-    java_class="com.airscholar.spark.WordCountJob",
-    dag=dag
-)
+# scala_job = SparkSubmitOperator(
+#     task_id="scala_job",
+#     conn_id="spark-conn",
+#     application="jobs/scala/target/scala-2.12/word-count_2.12-0.1.jar",
+#     dag=dag
+# )
+#
+# java_job = SparkSubmitOperator(
+#     task_id="java_job",
+#     conn_id="spark-conn",
+#     application="jobs/java/spark-job/target/spark-job-1.0-SNAPSHOT.jar",
+#     java_class="com.airscholar.spark.WordCountJob",
+#     dag=dag
+# )
 
 
 end = PythonOperator(
@@ -47,4 +47,4 @@ end = PythonOperator(
     dag=dag
 )
 
-start >> [python_job, scala_job, java_job] >> end
+start >> [python_job] >> end
---
+++ jobs/python/fashion_campus_repartitioning.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+import sys
+
+
+def main(args):
+    if len(args) < 3:
+        print("Usage: csv_to_bronze.py <input_path> <partition_size> <output_path>")
+        sys.exit(1)
+
+    print(args)
+    input_csv_path = args[0]
+    partition_size = int(args[1])
+    bronze_stage_path = args[2]
+
+    spark = SparkSession.builder \
+        .appName("CSV to Bronze Stage") \
+        .getOrCreate()
+
+    df = spark.read.csv(input_csv_path, header=True, inferSchema=True)
+
+    repartitioned_df = df.repartition(partition_size)
+
+    repartitioned_df.write \
+        .mode("overwrite") \
+        .format("csv") \
+        .save(bronze_stage_path)
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
